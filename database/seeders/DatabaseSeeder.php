123110de484b0373cb61fcd79213779d1753cb99
sandi <sandimaulidika@gmail.com>
2026-06-24 08:02:38 +0700

    refactor: update UserFactory to use faker methods and streamline DatabaseSeeder user creation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,8 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,12 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+        User::factory()->create([
             'name' => 'Test User',
             'email' => 'admin@gmail.com',
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' => bcrypt('admin'),
         ]);
 
         $this->call(CakeShopSeeder::class);
